dcc7642cf6a8f4f260edbd5d8db58955d78c8c38
Bruno Finger <bruno.k.finger@gmail.com>
2017-12-29 16:44:09 +0100

    Show following and followers list

--- src/js/app/github/directives/githubUserFollowing.js
+++ src/js/app/github/directives/githubUserFollowing.js
@@ -1,15 +1,31 @@
 (angular => {
   const app = angular.module('xyz.brunofinger.usercentric.github');
 
-  app.directive('githubUserFollowing', ['GitHubDAO', (GitHubDAO) => {
+  app.directive('githubUserPeople', ['GitHubDAO', (GitHubDAO) => {
     return {
-        templateUrl: '/partials/github/user/following.tmpl.html',
+        templateUrl: '/partials/github/user/people.tmpl.html',
         scope: {
           user: '='
         },
+        controllerAs: 'peopleCtrl',
         controller($scope) {
+          this.currentNav = 'following';
+
+          this.navTo = (where) => {
+            if (where === 'following' || where === 'followers') {
+              this.currentNav = where;
+            }
+          };
+
+          this.isShowingFollowing = () => this.currentNav === 'following';
+          this.isShowingFollowers = () => this.currentNav === 'followers';
+
           GitHubDAO.getFollowing($scope.user.login).then(({ data }) => {
             $scope.following = data;
+          });
+
+          GitHubDAO.getFollowers($scope.user.login).then(({ data }) => {
+            $scope.followers = data;
           });
         }
     };
